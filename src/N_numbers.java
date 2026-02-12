ea4304b317b0845d85b2d23138d4d02e2c4c24cd
juzerwithaz <juzersm7@gmail.com>
2026-02-12 11:04:27 +0530

    New branch check

--- src/N_numbers.java
+++ src/N_numbers.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class N_numbers {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+
+        sc.close();
+    }
 }
